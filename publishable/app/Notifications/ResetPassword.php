ecdde2d919896692cadff9e0822f0f742937494b
Stéphane Rouillé <steph.rouille@gmail.com>
2019-07-29 10:41:05 +0200

    Ajout Mdp perdu en extends

---
+++ publishable/app/Notifications/ResetPassword.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Notifications;
+
+use MediactiveDigital\MedKit\Notifications\ResetPassword as MedKitResetPassword;
+
+class ResetPassword extends MedKitResetPassword
+{
+
+}
---
+++ src/Notifications/ResetPassword.php
@@ -0,0 +1,46 @@
+<?php
+namespace MediactiveDigital\MedKit\Notifications;
+
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordContract;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ResetPassword extends ResetPasswordContract
+{
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable) {
+
+        $token = $this->token;
+
+        if (static::$toMailCallback) {
+
+            return call_user_func(static::$toMailCallback, $notifiable, $token);
+        }
+
+        $fullName = $notifiable->firstname . ' ' . $notifiable->name;
+        $email = $notifiable->email;
+
+        $date = '';
+
+        if (($passwordReset = DB::table('password_resets')->where('email', $email)->first()) && Hash::check($token, $passwordReset->token)) {
+
+            $date = date(_i('d/m/Y à H:i'), strtotime($passwordReset->created_at));
+        }
+
+        $link = url(route('back.password.reset', [$token, 'email=' . $email], false));
+
+        return (new MailMessage)
+            ->subject(_i('Demande de réinitialisation du mot de passe'))
+            ->view('emails.html.reset', compact('fullName', 'date', 'link'));
+    }
+}
