95d1dbab93c738fa404bf6852ab844c38bab03e3
Léo <l.picot@mediactive.fr>
2019-10-04 15:10:36 +0200

    Ajout de la release

--- publishable/config/sentry.php
+++ publishable/config/sentry.php
@@ -1,10 +1,15 @@
 <?php
+// Release sous la forme {$sha}@{$branch}
+// Le cache est géré par artisan config:cache
+$branch = trim(exec("cat ". base_path('.git') ."/HEAD | cut -d '/' -f 3"));
+$sha = substr(trim(exec("cat ". base_path('.git') ."/refs/heads/{$branch}")),0,7);
+
 
 return [
     'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
     'send_default_pii'=>true,
     // capture release as git sha
-    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
+    'release' => "{$sha}@{$branch}",
     'breadcrumbs' => [
         // Capture bindings on SQL queries logged in breadcrumbs
         'sql_bindings' => true,
