692d37cda62792123ccb19dd4ceb1bff833d526c
Aurelien de la Porte des Vaux <a.delaporte@mediactive.fr>
2019-12-11 18:55:34 +0100

    les seeedrs

--- publishable/database/seeds/DatabaseSeeder.php
+++ publishable/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             RolesTableSeeder::class,
+            PermissionsTableSeeder::class,
+            RoleHasPermissionsTableSeeder::class,
 	    ]);
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
