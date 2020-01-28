e52751e25f443cf8ffee82adf053a0991c843799
Aurelien de la Porte des Vaux <a.delaporte@mediactive.fr>
2020-01-28 18:31:28 +0100

    correction policies

--- publishable/app/Policies/MailTemplatePolicy.php
+++ publishable/app/Policies/MailTemplatePolicy.php
@@ -1,145 +1,141 @@
 <?php
+
 namespace App\Policies;
 
 use App\Models\User;
 use App\Models\MailTemplate;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class MailTemplatePolicy
-{
-    use HandlesAuthorization;
+class MailTemplatePolicy {
 
-    /**
-     * Determine whether the user can view any mailTemplate.
-     *
-     * @param \App\Models\User $user
-     * @return mixed
-     */
-    public function viewAny(User $user )
-    {
+	use HandlesAuthorization;
 
-        if ($user->can('mail-templates_view_own')) {
-             return true;
-        }
+	/**
+	 * Determine whether the user can view any mailTemplate.
+	 *
+	 * @param \App\Models\User $user
+	 * @return mixed
+	 */
+	public function viewAny(User $user) {
 
-        if ( $user->can('mail-templates_view_all') ) {
-            return true;
-        }
-    }
+		if ($user->can('mail-templates_view_all')) {
+			return true;
+		}
 
-    /**
-     * Determine whether the user can view the mailTemplate.
-     *
-     * @param \App\User $user
-     * @param App\Models\MailTemplate $mailTemplate
-     * @return mixed
-     */
-    public function view(User $user, MailTemplate $mailTemplate)
-    {
+		if ($user->can('mail-templates_view_own')) {
+			return true;
+		}
+	}
 
-         if ($user->can('mail-templates_view_own')) {
-            return $user->id == $mailTemplate->created_by;
-        }
+	/**
+	 * Determine whether the user can view the mailTemplate.
+	 *
+	 * @param \App\User $user
+	 * @param App\Models\MailTemplate $mailTemplate
+	 * @return mixed
+	 */
+	public function view(User $user, MailTemplate $mailTemplate) {
 
-        if ($user->can('mail-templates_view_all')) {
-            return true;
-        }
+		if ($user->can('mail-templates_view_all')) {
+			return true;
+		}
 
-        /**
-         * Exemple Si il y a une methode published ?
-         *
-        if ($mailTemplate->published) {
-            return true;
-        }
-        // visitors cannot view unpublished items
-        if ($user === null) {
-            return false;
-        }
-        // admin overrides published status
-        if ($user->can('view unpublished permissions')) {
-            return true;
-        }
-        // authors can view their own unpublished permissions
-        return $user->id == $mailTemplate->created_by;
-         */
-    }
-    /**
-     * Determine whether the user can create mailTemplate.
-     *
-     * @param \App\User $user
-     * @return mixed
-     */
-    public function create(User $user )
-    {
+		if ($user->can('mail-templates_view_own')) {
+			return $user->id == $mailTemplate->created_by;
+		}
 
-        if ($user->can('mail-templates_create')) {
-            return true;
-        }
-    }
+		/**
+		 * Exemple Si il y a une methode published ?
+		 *
+		  if ($mailTemplate->published) {
+		  return true;
+		  }
+		  // visitors cannot view unpublished items
+		  if ($user === null) {
+		  return false;
+		  }
+		  // admin overrides published status
+		  if ($user->can('view unpublished permissions')) {
+		  return true;
+		  }
+		  // authors can view their own unpublished permissions
+		  return $user->id == $mailTemplate->created_by;
+		 */
+	}
 
-    /**
-     * Determine whether the user can update the mailTemplate.
-     *
-     * @param \App\User $user
-     * @param App\Models\MailTemplate $mailTemplate
-     * @return mixed
-     */
-    public function update(User $user, MailTemplate $mailTemplate)
-    {
+	/**
+	 * Determine whether the user can create mailTemplate.
+	 *
+	 * @param \App\User $user
+	 * @return mixed
+	 */
+	public function create(User $user) {
 
-         if ($user->can('mail-templates_edit_own')) {
-            return $user->id == $mailTemplate->created_by;
-        }
+		if ($user->can('mail-templates_create')) {
+			return true;
+		}
+	}
 
-        if ($user->can('mail-templates_edit_all')) {
-            return true;
-        }
-    }
+	/**
+	 * Determine whether the user can update the mailTemplate.
+	 *
+	 * @param \App\User $user
+	 * @param App\Models\MailTemplate $mailTemplate
+	 * @return mixed
+	 */
+	public function update(User $user, MailTemplate $mailTemplate) {
 
-    /**
-     * Determine whether the user can delete the mailTemplate.
-     *
-     * @param \App\User $user
-     * @param App\Models\MailTemplate $mailTemplate
-     * @return mixed
-     */
-    public function delete(User $user, MailTemplate $mailTemplate)
-    {
+		if ($user->can('mail-templates_edit_all')) {
+			return true;
+		}
 
-                if ($user->can('mail-templates_delete_own')) {
-            return $user->id == $mailTemplate->created_by;
-        }
+		if ($user->can('mail-templates_edit_own')) {
+			return $user->id == $mailTemplate->created_by;
+		}
+	}
 
-        if ($user->can('mail-templates_delete_any')) {
-            return true;
-        }
-    }
+	/**
+	 * Determine whether the user can delete the mailTemplate.
+	 *
+	 * @param \App\User $user
+	 * @param App\Models\MailTemplate $mailTemplate
+	 * @return mixed
+	 */
+	public function delete(User $user, MailTemplate $mailTemplate) {
 
-    /**
-     * Determine whether the user can restore the mailTemplate.
-     *
-     * @param \App\Models\User $user
-     * @param App\Models\MailTemplate mailTemplate
-     * @return mixed
-    public function restore(User $user, MailTemplate $mailTemplate)
-    {
+		if ($user->can('mail-templates_delete_any')) {
+			return true;
+		}
 
-        //
-        dd('restore');
-    }
-     */
+		if ($user->can('mail-templates_delete_own')) {
+			return $user->id == $mailTemplate->created_by;
+		}
+	}
 
-    /**
-     * Determine whether the user can permanently delete the mailTemplate.
-     *
-     * @param \App\Models\User $user
-     * @param App\Models\MailTemplate mailTemplate
-     * @return mixed
-    public function forceDelete(User $user, MailTemplate $mailTemplate)
-    {
+	/**
+	 * Determine whether the user can restore the mailTemplate.
+	 *
+	 * @param \App\Models\User $user
+	 * @param App\Models\MailTemplate mailTemplate
+	 * @return mixed
+	  public function restore(User $user, MailTemplate $mailTemplate)
+	  {
 
-        //
-        dd('forceDelete');
-    }
-     */
+	  //
+	  dd('restore');
+	  }
+	 */
+	/**
+	 * Determine whether the user can permanently delete the mailTemplate.
+	 *
+	 * @param \App\Models\User $user
+	 * @param App\Models\MailTemplate mailTemplate
+	 * @return mixed
+	  public function forceDelete(User $user, MailTemplate $mailTemplate)
+	  {
+
+	  //
+	  dd('forceDelete');
+	  }
+	 */
 }
--- publishable/app/Policies/UserPolicy.php
+++ publishable/app/Policies/UserPolicy.php
@@ -1,151 +1,142 @@
 <?php
+
 namespace App\Policies;
- 
-use App\Models\User; 
+
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class UserPolicy
-{
-    use HandlesAuthorization;
-	
-	/**  
-     * Determine whether the user can view any user.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user )
-    { 
-		 
-		
-if ($user->can('users_view_own')) {
-             return true;
-        }
- 
-        if ( $user->can('users_view_all') ) { 
-            return true;
-        } 
-    }
-	
-    /**
-     * Determine whether the user can view the user.
-     *
-     * @param  \App\User $user
-     * @param  App\Models\User $user
-     * @return mixed
-     */
-    public function view(User $u, User $user )
-    {    
-		
- if ($u->can('users_view_own')) {
-            return $u->id == $user->created_by;
-        }
-		
-        if ($u->can('users_view_all')) {
-            return true;
-        }
-		
+class UserPolicy {
+
+	use HandlesAuthorization;
+
+	/**
+	 * Determine whether the user can view any user.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @return mixed
+	 */
+	public function viewAny(User $user) {
+
+		if ($user->can('users_view_all')) {
+			return true;
+		}
+
+
+		if ($user->can('users_view_own')) {
+			return true;
+		}
+	}
+
+	/**
+	 * Determine whether the user can view the user.
+	 *
+	 * @param  \App\User $user
+	 * @param  App\Models\User $user
+	 * @return mixed
+	 */
+	public function view(User $u, User $user) {
+
+		if ($u->can('users_view_all')) {
+			return true;
+		}
+
+		if ($u->can('users_view_own')) {
+			return $u->id == $user->created_by;
+		}
+
 		/**
 		 * Exemple Si il y a une methode published ?
-		 * 
-        if ($user->published) {
-            return true;
-        }
-        // visitors cannot view unpublished items
-        if ($user === null) {
-            return false;
-        }
-        // admin overrides published status
-        if ($user->can('view unpublished permissions')) {
-            return true;
-        }
-        // authors can view their own unpublished permissions
-        return $user->id == $user->created_by; 
+		 *
+		  if ($user->published) {
+		  return true;
+		  }
+		  // visitors cannot view unpublished items
+		  if ($user === null) {
+		  return false;
+		  }
+		  // admin overrides published status
+		  if ($user->can('view unpublished permissions')) {
+		  return true;
+		  }
+		  // authors can view their own unpublished permissions
+		  return $user->id == $user->created_by;
 		 */
-		
-    }
-    /**
-     * Determine whether the user can create user.
-     *
-     * @param  \App\User $user
-     * @return mixed
-     */
-    public function create(User $user )
-    {	
-		
-        if ($user->can('users_create')) {
-            return true;
-        }
-    }
-	
-	
-    /**
-     * Determine whether the user can update the user.
-     *
-     * @param  \App\User $user
-     * @param  App\Models\User $user
-     * @return mixed
-     */
-    public function update(User $u, User $user )
-    {  
-		
-		
- if ($u->can('users_edit_own')) {
-            return $u->id == $user->created_by;
-        }
-		
-        if ($u->can('users_edit_all')) {
-            return true;
-        }
-    }
-	
-    /**
-     * Determine whether the user can delete the user.
-     *
-     * @param  \App\User $user
-     * @param  App\Models\User $user
-     * @return mixed
-     */
-    public function delete(User $u, User $user )
-    {
-		
-		 
+	}
 
-        if ($u->can('users_delete_own')) {
-            return $u->id == $user->created_by;
-        } 
+	/**
+	 * Determine whether the user can create user.
+	 *
+	 * @param  \App\User $user
+	 * @return mixed
+	 */
+	public function create(User $user) {
 
-		
-        if ($u->can('users_delete_any')) {
-            return true;
-        }
-    }
-	
-    /**
-     * Determine whether the user can restore the user.
-     *
-     * @param  \App\Models\User  $user
-     * @param  App\Models\User user
-     * @return mixed
-    public function restore(User $user, User $user)
-    {
-		
-        //
-		dd('restore');
-    }
-     */
+		if ($user->can('users_create')) {
+			return true;
+		}
+	}
 
-    /**
-     * Determine whether the user can permanently delete the user.
-     *
-     * @param  \App\Models\User  $user
-     * @param  App\Models\User user
-     * @return mixed
-    public function forceDelete(User $user, User $user)
-    {
-		
-        //
-		dd('forceDelete');
-    }
-     */
+	/**
+	 * Determine whether the user can update the user.
+	 *
+	 * @param  \App\User $user
+	 * @param  App\Models\User $user
+	 * @return mixed
+	 */
+	public function update(User $u, User $user) {
+
+		if ($u->can('users_edit_all')) {
+			return true;
+		}
+
+
+		if ($u->can('users_edit_own')) {
+			return $u->id == $user->created_by;
+		}
+	}
+
+	/**
+	 * Determine whether the user can delete the user.
+	 *
+	 * @param  \App\User $user
+	 * @param  App\Models\User $user
+	 * @return mixed
+	 */
+	public function delete(User $u, User $user) {
+
+		if ($u->can('users_delete_any')) {
+			return true;
+		}
+ 
+		if ($u->can('users_delete_own')) {
+			return $u->id == $user->created_by;
+		}
+	}
+
+	/**
+	 * Determine whether the user can restore the user.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  App\Models\User user
+	 * @return mixed
+	  public function restore(User $user, User $user)
+	  {
+
+	  //
+	  dd('restore');
+	  }
+	 */
+	/**
+	 * Determine whether the user can permanently delete the user.
+	 *
+	 * @param  \App\Models\User  $user
+	 * @param  App\Models\User user
+	 * @return mixed
+	  public function forceDelete(User $user, User $user)
+	  {
+
+	  //
+	  dd('forceDelete');
+	  }
+	 */
 }
