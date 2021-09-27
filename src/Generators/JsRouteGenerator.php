c88a371aa214fac8c43196161f42dcbc0db62df4
fchalal <farand.chalal@mediactivegroup.com>
2021-09-27 17:07:42 +0200

    Fix JS routes

--- src/Generators/JsRouteGenerator.php
+++ src/Generators/JsRouteGenerator.php
@@ -2,15 +2,34 @@
 
 namespace MediactiveDigital\MedKit\Generators;
 
+use MediactiveDigital\MedKit\Traits\Reflection;
+
 use Tightenco\Ziggy\Ziggy;
 
 class JsRouteGenerator extends Ziggy {
 
-    public function __construct($group = null) {
+    use Reflection;
 
-        parent::__construct($group);
+    protected $port;
+
+    public function __construct($group = null, string $url = null) {
+
+        parent::__construct($group, $url);
 
         $this->url = 'window.location.origin';
         $this->port = 'window.location.port';
+    }
+
+    /**
+     * Convert this Ziggy instance to an array.
+     */
+    public function toArray(): array {
+
+        return [
+            'url' => $this->url,
+            'port' => $this->port,
+            'defaults' => method_exists(app('url'), 'getDefaultParameters') ? app('url')->getDefaultParameters() : [],
+            'routes' => $this->callReflectionMethod('applyFilters', $this->group)->toArray()
+        ];
     }
 }
